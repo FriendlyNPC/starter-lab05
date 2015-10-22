f365be752d149624218cc65d53762b37115d6b29
FriendlyNPC <Devanyim@gmail.com>
2015-10-22 16:09:45 -0700

    part 4 admin controller

---
+++ application/controllers/Admin.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Admin extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->data['title'] = 'Quotations Maintenance';
+        $this->data['quotes'] = $this->quotes->all();
+        $this->data['pagebody'] = 'admin_list';
+        $this->render();
+    }
+
+}
